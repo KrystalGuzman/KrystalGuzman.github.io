d8544dc99720b8c9920e0240e590020760fbbe75
Krystal Guzman <kryguzman@gmail.com>
2020-03-30 15:26:07 -0700

    Updates

---
+++ precache-manifest.e2c182917f6bbf5b70663940d334a578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858047e7b7dce3f9d1f84a9f7646767e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebfd49b8a46475133a8b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bdcf8116b1a222ee7450",
+    "url": "/static/js/2.c4a4b24a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4a4b24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfd49b8a46475133a8b",
+    "url": "/static/js/main.f7c9441e.chunk.js"
+  },
+  {
+    "revision": "166cf5f8ec0cbaf35406",
+    "url": "/static/js/runtime-main.d8445b1d.js"
+  },
+  {
+    "revision": "0da9295cbbb582f49472cece25883331",
+    "url": "/static/media/Kheadshot.0da9295c.jpg"
+  }
+]);
